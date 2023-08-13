b4f60fb8dcaf7e15e003900ebe21a4ca378cc755
azharrahad31 <azherudinrahad@gmail.com>
2023-08-13 18:16:06 +0600

    registration form complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import Registration from './Pages/Registration';
 
 function App() {
-  const [count, setCount] = useState(0);
+
   const route=createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<Registration></Registration>}></Route>
   ))
 
   return (
+    <RouterProvider router={route}></RouterProvider>
    
   )
 }
--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import Image from '../Components/Image';
+import reg from "../assets/reg.png";
+import { Button, TextField, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
     return (
-        <div>
+        <div className='authenticationPage'>
+            <div className='left'>
+                <div className='text-container'>
+                    <h1>Get started with easily register</h1>
+
+                    <p>Free register and you can enjoy it</p>   
+
+                    <TextField type='text' id="outlined-basic" label="FirstName" variant="outlined" className='inputCss'/>
+
+                    <TextField type='text' id="outlined-basic" label="Email" variant="outlined"  className='inputCss'/>
+
+                    <TextField type='password' id="outlined-basic" label="Password" variant="outlined"  className='inputCss'/>
+
+                    <Button variant="contained" className='SignUpBtn'>Sign up</Button>
+                    <Typography variant="p" component="p" className='semiText'>Already have an account ? <Link className='orange'>Sign In</Link></Typography>
+                </div>
+            </div>
+            <div className='right'>
+                <Image ImgSrc={reg} className={"bg"}></Image>
+            </div>
             
         </div>
     );
